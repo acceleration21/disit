04510e1e0230c4f2658b62205528263eba35d4c7
wangjunze <171635094@qq.com>
2019-01-17 11:06:27 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("欢迎进入专车管理系统\n");
+
+    while (1)
+    {
+        printf("1--添加车辆位置\n");
+        printf("2--删除车辆位置\n");
+        printf("3--显示全部车辆位置\n");
+        printf("4--返回离我位置的最近车辆\n");
+        printf("5--退出");
+
+        printf("请输入相应的功能编码\n");
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+            
+        }
+         if (code == 2)
+        {
+            
+        }
+         if (code == 3)
+        {
+            
+        }
+         if (code == 4)
+        {
+            
+        }
+         if (code == 5)
+        {
+            printf("确认退出，回车继续\n");
+            break;
+        }
+    }
 
     return 0;
-
 }
